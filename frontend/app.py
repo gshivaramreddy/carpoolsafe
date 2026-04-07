3f20995be1bc8f0886e87c22d2f61814c6f71b07
Shivaram <shivaramreddy422@gmail.com>
2026-04-07 18:01:42 +0530

    final frontend connect

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,1 @@
-import streamlit as st
-import requests
-
-st.title("🚗 CarpoolSafe")
-
-if st.button("Check Backend"):
-    try:
-        res = requests.get("https://carpoolsafe.onrender.com")
-        st.success("Backend working ✅")
-    except:
-        st.error("Backend failed ❌")
+from main_app import *
