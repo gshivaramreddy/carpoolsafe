61e010d8ccab991d5a30f3d166f16061fb0ba2bf
Shivaram <shivaramreddy422@gmail.com>
2026-04-09 00:17:19 +0530

    fix indentation in render_auth

--- frontend/main_app.py
+++ frontend/main_app.py
@@ -177,9 +177,9 @@ def render_auth():
     if st.button("Login"):
         st.write("Clicked login")
 
-    # ── SIGN IN TAB ─────────
-    # ──────────────────────────────────────────────────
+    # tabs
     tab_login, tab_signup = st.tabs(["Login", "Create Account"])
+
     with tab_login:
         st.markdown("<br>", unsafe_allow_html=True)
         li_email = st.text_input("Email address", placeholder="you@example.com", key="li_email")
